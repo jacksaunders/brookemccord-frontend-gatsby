5cbebf8a2ca94360964a2dbcda7a100722e7afb2
Jack Saunders <j.saunders@iwoca.co.uk>
2019-03-29 21:37:23 +0000

    add initial home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
+import styles from './index.module.css'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={styles.wrapper}>
+      <h1>Brooke McCord</h1>
+      <h2>
+        <span>Digital Editor of The Face</span> / Writer / Creative Consultant
+      </h2>
+      <p>
+        Brooke McCord is a London-based writer, editor, and consultant, working
+        across fashion and contemporary culture who has contributed to Dazed &
+        Confused, i-D, Office, The Guardian, Dazed Beauty, LOVE, Wonderland and
+        AnOther. As a digital native, Brooke has translated her knowledge and
+        understanding of digital behaviour to strategy and content projects for
+        the likes of adidas Originals, Nike, Liberty of London, Floom,
+        Glossier, The Body Shop, Flannels, Matches Fashion, 3INA, Ace & Tate and
+        Topshop.
+      </p>
+      <div className={styles.contact}>
+        <div>
+          <h4>Email</h4>
+          <a href="mailto:brooke@brookemccord.com">brooke@brookemccord.com</a>
+        </div>
+        <div>
+          <h4>Instagram</h4>
+          <a href="https://instagram.com/brooke_mccord" target="_blank">
+            @brooke_mccord
+          </a>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
